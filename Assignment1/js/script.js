0902b6028d4405a84ea0a8539be332f8c6372d9d
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-02-12 07:33:08 -0800

    1 - 10 completed

---
+++ Assignment1/js/script.js
@@ -0,0 +1,65 @@
+// 1. Convert to Camel Case notation:
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// 2. An example of a numeric, string, Boolean and null literal expression:
+// 10
+// 'string'
+// true
+// null
+
+// 3. 2 examples of complex / variable expressions
+// let myTotal = 3 + 7;
+// let sum = 3 * 6;
+
+// 4. declare but not assign 9 variables to:
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let intZipCode;
+// let intYourAge;
+// let strReferralSource;
+// let blnMayWeContactYou;
+
+// 5. Assign values to 3 of the 9 variables above
+// let strFirstName = 'Michelle';
+// let intYourAge = 51;
+// let blnMayWeContactYou = false;
+
+// 6. Create a variable. Add a num and str and display coerced result in the console
+// let myAge = 50;
+// console.log("Michelle's age is " + myAge + ".")
+
+// 7. Create 2 variables. 1st variable add a bln and a str and display result. 2nd variable add a num and a bln and display result.
+
+// let fName = "Michelle"
+// console.log(fName + " is my name " + true)
+
+// let myAge = 50
+// console.log(myAge + true)
+
+// 8. is the string literal valid? if not, what's the fix?
+// No, not valid
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// 9. Create 2 variables - 1 that produces a null value and 1 that produces an undefined value in console
+// let myName = null
+// console.log(myName)
+// let yourName
+// console.log(yourName)
+
+// 10. Use unary typeof on string, num, bool, object and undefined literals to return their types in console.
+
+// console.log(typeof ('three'))
+// console.log(typeof (3))
+// console.log(typeof (true))
+// console.log(typeof (null))
+// console.log(typeof(x))
+
+// 11. 
