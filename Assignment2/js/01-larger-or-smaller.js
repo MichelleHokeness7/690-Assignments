6db462f044d92aef2b153b20621b3e702bcfdd23
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-02-22 11:06:09 -0800

    changes made to larger or smaller

--- Assignment2/js/01-larger-or-smaller.js
+++ Assignment2/js/01-larger-or-smaller.js
@@ -1,8 +1,10 @@
-let smallNum = parseInt(prompt('Enter a number between 1 and 10'));
-let largeNum = parseInt(prompt('Enter another number between 1 and 10'));
+let firstNum = parseInt(prompt('Enter a number'));
+let secondNum = parseInt(prompt('Enter another number'));
 
-if (smallNum < largeNum) {
-    document.write(`${largeNum} is the larger number you entered.`)
-} else if (smallNum == largeNum) {
+if (firstNum < secondNum) {
+    document.write(`${secondNum} is the larger number you entered.`)
+} else if (firstNum === secondNum) {
     document.write('The 2 numbers you entered were the same.')
+} else if (secondNum < firstNum) {
+    document.write(`${firstNum} is the larger number you entered.`)
 }
