22782bf7ac2ccbaaba79b23bfa0898ac351d8c50
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-02-22 10:53:49 -0800

    commit changes to word translator - console.log

--- Assignment2/js/02-word-translator.js
+++ Assignment2/js/02-word-translator.js
@@ -1,13 +1,13 @@
 let userInput = prompt('Enter a language code of choice from the following: "es", "de", "en" or "fr"').toLocaleLowerCase()
 
 if (userInput === "de") {
-    document.write('Hello World translated in German is: Hallo Welt')
+    console.log('Hello World translated in German is: Hallo Welt')
 } else if (userInput === "es") {
-    document.write('Hello World translated in Spanish is: Holla Mundo')
+    console.log('Hello World translated in Spanish is: Holla Mundo')
 } else if (userInput === "en") {
-    document.write('Hello World')
+    console.log('Hello World')
 } else if (userInput === "fr") {
-    document.write('Hello World translated in French is: Bonjour le monde')
+    console.log('Hello World translated in French is: Bonjour le monde')
 } else {
-    document.write('Hello World')
+    console.log('Hello World')
 }
--- Assignment2/js/03-coinflip.js
+++ Assignment2/js/03-coinflip.js
@@ -2,16 +2,16 @@ let coinFlip = Math.round(Math.random())
 let choice = prompt('Enter "Heads" or "Tails"').toLocaleLowerCase()
 
 if (choice === 'heads') {
-    if (coinFlip <= 0) {
+    if (coinFlip === 0) {
         alert('The flip was heads and you chose heads... You win!')
-    } else if (coinFlip > 0) {
+    } else if (coinFlip === 1) {
         alert('The flip was tails and you chose heads... You lose.')
     }
 
 } else if (choice === 'tails') {
-    if (coinFlip <= 0) {
+    if (coinFlip === 0) {
         alert('The flip was heads but you chose tails... You lose.')
-    } else if (coinFlip > 0) {
+    } else if (coinFlip === 1) {
         alert('The flip was tails and you chose tails... You win!')
     }
 
