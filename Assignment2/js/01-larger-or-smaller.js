744debd270525248f45ae7fc04931ad71745dce1
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-02-22 12:03:05 -0800

    larger smaller if nums are equal moved to end

--- Assignment2/js/01-larger-or-smaller.js
+++ Assignment2/js/01-larger-or-smaller.js
@@ -3,8 +3,8 @@ let secondNum = parseInt(prompt('Enter another number'));
 
 if (firstNum < secondNum) {
     document.write(`${secondNum} is the larger number you entered.`)
-} else if (firstNum === secondNum) {
-    document.write('The 2 numbers you entered were the same.')
 } else if (secondNum < firstNum) {
     document.write(`${firstNum} is the larger number you entered.`)
-}
+} else if (firstNum === secondNum) {
+    document.write('The 2 numbers you entered were the same.')
+} 
