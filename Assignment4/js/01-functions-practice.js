51e3bf5df2a3d4b061e2220bcff06cf44ee15c07
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-03-07 10:27:02 -0800

    added practice functions

--- Assignment4/js/01-functions-practice.js
+++ Assignment4/js/01-functions-practice.js
@@ -1,9 +1,26 @@
 //STEP 1
-
+// let num = 7
+// function halfNumber() {
+//     return num / 2
+// }
+// console.log(`Half of ${num} is ${halfNumber()}`)
 //STEP 2
-
+// let num = 7
+// function squareNumber() {
+//     return num * num
+// }
+// console.log(`The result of squaring ${num} is ${squareNumber()}`)
 //STEP 3
-
+// let numOne = 9
+// let numTwo = 36
+// function percentOf() {
+//     return numOne / numTwo * 100
+// }
+// console.log(`${numOne} is ${percentOf()}% of ${numTwo}`)
 //STEP 4
-
-//STEP 5
+// let numOne = 15
+// let numTwo = 4
+// function findModulus() {
+//     return numOne % numTwo
+// }
+// console.log(`${findModulus()} is the modulus of ${numOne} and ${numTwo} `)
