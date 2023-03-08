8c78668655deb477d0e90a599c8d6810278553a1
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-03-07 16:04:46 -0800

    added modules

--- Assignment4/js/03-basic-calculator.js
+++ Assignment4/js/03-basic-calculator.js
@@ -1,9 +1,28 @@
 // IMPORT THE MODULE
-
+import { add, subtract, multiply, divide } from './modules/calculator';
 
 // COLLECT FIRST NUMBER FROM USER
+let userNum1 = Number(prompt('Enter a number'))
 // COLLECT SECOND NUMBER FROM USER
+let userNum2 = Number(prompt('Enter another number'))
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
-
+let userOp = prompt('Enter \'+\',\'-\', \'*\', \'/\' to calculate the numbers')
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
+switch (userOp) {
+    case '+':
+        console.log(add())
+        break;
+    case '-':
+        console.log(calc.subtract())
+        break;
+    case '*':
+        console.log(calc.multiply())
+        break;
+    case '/':
+        console.log(calc.divide())
+        break;
+    default:
+        alert('You must enter \'+\', \'-\', \'*\', \'/\'')
+        break;
+}
 // CALL THE APPROPRIATE FUNCTION
--- Assignment4/js/modules/calculator.js
+++ Assignment4/js/modules/calculator.js
@@ -1,13 +1,24 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
+const calculate = () => {
 
+}
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+const add = () => {
+    return userNum1 + userNum2
+}
 // SUBTRACT FUNCTION
-
+const subtract = () => {
+    return userNum1 - userNum2
+}
 // MULTIPLY FUNCTION
-
+const multiply = () => {
+    return userNum1 * userNum2
+}
 // DIVIDE FUNCTION
-
+const divide = () => {
+    return userNum1 / userNum2
+}
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export { add, subtract, multiply, divide };
