35b5b550702459e1170c34730a194324c3ff34a5
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-03-14 15:56:36 -0700

    commit html, css

---
+++ Assignment5/js/script.js
@@ -0,0 +1,11 @@
+// CREATE GLOBAL VARIABLES
+let empForm
+let id
+let name
+let ext
+let email
+let department
+let submit
+
+// CREATE HELPER FUNCTION 
+const $ = (id) => document.getElementById(id)
