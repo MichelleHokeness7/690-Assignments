8c480bdd91093d8c8ab13a65c7f486d0adc25de5
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-03-14 16:59:15 -0700

    added script changes

--- Assignment5/js/script.js
+++ Assignment5/js/script.js
@@ -1,11 +1,22 @@
-// CREATE GLOBAL VARIABLES
-let empForm
-let id
-let name
-let ext
-let email
-let department
-let submit
+// CREATE GLOBAL VARIABLES with HELPER FUNCTION
+    const $ = (id) => document.getElementById(id)
 
-// CREATE HELPER FUNCTION 
-const $ = (id) => document.getElementById(id)
+    let empForm = $('empForm')
+    let id = $('id')
+    let empName = $('empName')
+    let ext = $('ext')
+    let email = $('email')
+    let department = $('department')
+    let submit = $('submit')
+
+    empForm.addEventListener('submit', empInfo)
+
+    function empInfo(e) {
+        e.preventDefault()
+        console.log(`ID: ${id.value}`)
+        console.log(`Name: ${empName.value}`)
+        console.log(`Extension: ${ext.value}`)
+        console.log(`Email: ${email.value}`)
+        console.log(`Department: ${department.value}`)
+    }
+
