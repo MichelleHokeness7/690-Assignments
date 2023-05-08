b9cd59b26a4fcaa78e4d6a67b8c058df5dfc27c6
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-05-08 16:32:05 -0700

    added strings.js

--- M11-Assignment/js/strings.js
+++ M11-Assignment/js/strings.js
@@ -1,19 +1,38 @@
 //STEP 1
-
+// let userName = prompt('Enter your name')
+// alert(`Your name's length of characters is ${userName.length}.`)
 //STEP 2
-
+// let userName = prompt('Enter your name')
+// alert(`Your name's length of characters is ${userName.length}.`)
+// let userNum = prompt('Enter a number from 0 to the length given to know the specific letter')
+// alert(`You entered the number ${userNum.valueOf()} which lands on the letter ${userName.charAt(userNum)}`)
 //STEP 3
-
+// let firstName = prompt('Enter your first name')
+// let lastName = prompt('Enter your last name')
+// alert(`Your name is ${firstName.concat(lastName)}`)
 //STEP 4
-
+// let text = 'The quick brown fox jumps over the lazy dog'
+// alert(`The index of the word 'fox' is at ${text.indexOf('fox')}`)
 //STEP 5
-
+// let text = 'The quick brown fox jumps over the lazy fox'
+// alert(`The index of the word 'fox' is at ${text.lastIndexOf('fox')}`)
 //STEP 6
-
-//STEP 7
-
+// let text = 'The quick brown fox jumped over the lazy dog'
+// let userName = prompt('Please enter your full name')
+// alert(text.replace('the lazy dog', userName))
+// //STEP 7
+// let text = 'The quick brown fox jumped over the lazy dog'
+// let usersWord = prompt(`Enter a word from the following text, "${text}"`)
+// alert(text.search(usersWord))
 //STEP 8
-
+// let old_string = 'The quick brown fox jumped over the lazy dog'
+// let new_string = old_string.slice(31, 44)
+// alert(new_string)
 //STEP 9
-
+// let text = ' THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG          '
+// alert(text.trim().toLocaleLowerCase())
 //STEP 10
+// let text = 'the quick brown fox jumped over the lazy dog'
+// alert(text.replace('the q', 'The q'))
+
+
