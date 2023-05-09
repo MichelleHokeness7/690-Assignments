e7a55054f584c141d1d332110e9b0844281ef30c
Michelle Hokeness <hokenessmichelle@gmail.com>
2023-05-09 16:19:38 -0700

    added math.js

--- M11-Assignment/js/math.js
+++ M11-Assignment/js/math.js
@@ -1,9 +1,20 @@
 //STEP 1
-
+// let userNum = prompt('Enter a number')
+// console.log(Math.abs(userNum))
 //STEP 2
-
+// let userNum = prompt('Enter a number that has a decimal in it')
+// console.log(Math.ceil(userNum))
 //STEP 3
-
+// let userNum = prompt('Enter a number that has a decimal in it')
+// console.log(Math.floor(userNum))
 //STEP 4
-
+// let userInput = prompt("Enter 5 numbers separated by commas: ex 1,2,3,4,5")
+// let numbers = userInput.split(",").map(Number)
+// let max = Math.max(...numbers)
+// let min = Math.min(...numbers)
+// console.log(`The minimum number is: ${min} \nThe maximum number is: ${max}`);
 //STEP 5
+// const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// let d = new Date()
+// let dayName = weekday[d.getDay()].slice(0, 1)
+// console.log(dayName)
